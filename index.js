9c05dfb7bd9fd87a853bb0be0fd1bbbc72715b20
Han <han.xiao@hanxstandard.com>
2016-12-30 10:20:14 -0500

    adding mongoose

--- index.js
+++ index.js
@@ -7,18 +7,12 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.listen((process.env.PORT || 3000));
 
-
-
-app.use(webhook);
-
-
-
-/*
 // Server frontpage
 app.get('/', function (req, res) {
     res.send('This is TestBot Server');
 });
-
+app.use(webhook);
+/*
 // Facebook Webhook
 app.get('/webhook', function (req, res) {
     if (req.query['hub.verify_token'] === 'testbot_verify_token') {
