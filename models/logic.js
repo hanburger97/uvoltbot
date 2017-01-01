8486fc4e5c8c5ebd37b60e369a00edd6b324faae
Han <han.xiao@hanxstandard.com>
2017-01-01 13:02:19 -0500

    a

--- models/logic.js
+++ models/logic.js
@@ -19,7 +19,7 @@ function sendMessage(recipientId, message) {
     });
 };
 function getUserInfo(id){
-    request({
+    return request({
         url: 'https://graph.facebook.com/v2.6/' + id + '?fields=first_name,last_name,profile_pic,locale,timezone,gender',
         qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
         method: 'GET'
