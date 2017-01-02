423126548716e248d57c244f231ecf23f2228121
Han <han.xiao@hanxstandard.com>
2017-01-02 10:55:21 -0500

    a

--- routes/webhook.js
+++ routes/webhook.js
@@ -17,11 +17,13 @@ router.post('/webhook', function (req, res) {
     for (i = 0; i < events.length; i++) {
         var event = events[i];
         if (event.message && event.message.text) {
+
             event.message.text = event.message.text.toLowerCase();
             var words = event.message.text.split(' ');
             //console.log(words);
-            no_reply = true;
+
             for(z =0; z < words.length; z ++) {
+                no_reply = false;
                 var word = words[z];
                 Response.findOne({
                     trigger: word
@@ -31,49 +33,35 @@ router.post('/webhook', function (req, res) {
                     }
 
                     else if (!data) {
-                        console.log('No data')
-                        /*
-                        logic.sendMessage(event.sender.id, {
-                            text: "Sorry, I am not programmed to understand this yet",
-                            //text: "Sorry " + currentUser.first_name + ", I am not programmed to understand this yet",
-                            quick_replies: [
-                                {
-                                    content_type: "text",
-                                    title: "Ok",
-                                    payload: "undefined"
-                                },
-                                {
-                                    content_type: "text",
-                                    title: 'Nevermind'
-                                    payload: "start"
-                                }
-                            ]
-                        });*/
+                        console.log('No data');
+                        no_reply = true;
                     }
                     else {
                         logic.sendMessage(event.sender.id, data.response);
                         no_reply = false;
+                        
                     }
                 });
+                if (z+1 == words.length && no_reply){
+                    logic.sendMessage(event.sender.id, {
+                        text: "Sorry, I am not programmed to understand this yet",
+                        //text: "Sorry " + currentUser.first_name + ", I am not programmed to understand this yet",
+                        quick_replies: [
+                            {
+                                content_type: "text",
+                                title: "Ok",
+                                payload: "undefined"
+                            },
+                            {
+                                content_type: "text",
+                                title: 'Nevermind',
+                                payload: "start"
+                            }
+                        ]
+                    });
+                }
             };
-            if (no_reply){
-                logic.sendMessage(event.sender.id, {
-                    text: "Sorry, I am not programmed to understand this yet",
-                    //text: "Sorry " + currentUser.first_name + ", I am not programmed to understand this yet",
-                    quick_replies: [
-                        {
-                            content_type: "text",
-                            title: "Ok",
-                            payload: "undefined"
-                        },
-                        {
-                            content_type: "text",
-                            title: 'Nevermind',
-                            payload: "start"
-                        }
-                    ]
-                });
-            }
+
         } else if (event.postback){
 
             Postback.findOne({
