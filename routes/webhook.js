803287b9473e8ec042485dff2ce7b038363302f8
Elic Ng <elicng@gmail.com>
2017-01-06 20:32:04 -0500

    Add users to a temp list of paused users

--- routes/webhook.js
+++ routes/webhook.js
@@ -6,6 +6,8 @@ var Response = mongoose.model('Response');
 var Postback = mongoose.model('Postback');
 var async = require('async');
 
+var pausedUsers = []
+
 router.get('/webhook', function (req, res) {
   if (req.query['hub.verify_token'] === 'testbot_verify_token') {
     res.send(req.query['hub.challenge']);
@@ -15,8 +17,20 @@ router.get('/webhook', function (req, res) {
 });
 router.post('/webhook', function (req, res) {
   var events = req.body.entry[0].messaging;
+
   for (i = 0; i < events.length; i++) {
+
     var event = events[i];
+
+    if (pausedUsers[event.sender.id] && pausedUsers[event.sender.id] > new Date()) {
+      // skiped
+      logic.sendMessage(event.sender.id, {text: "DEBUG: PAUSED until " + pausedUsers[event.sender.id].toString()});
+      break
+    } else {
+      // Expired. Remove pause
+      delete pausedUsers[event.sender.id]
+    }
+
     if (event.postback) {
 
       Postback.findOne({
@@ -74,6 +88,11 @@ router.post('/webhook', function (req, res) {
               }
             }
             else {
+              var jsonData = JSON.parse(data.response);
+              if (jsonData.action && jsonData.action.pause) {
+                var until = new Date(new Date().getTime() + 20*60000);
+                pausedUsers[event.sender.id] = until
+              }
               logic.sendMessage(event.sender.id, data.response);
 
 
